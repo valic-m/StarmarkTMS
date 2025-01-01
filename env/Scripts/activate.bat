5a911b6a62c4ba8f6aeddf7076db731df96d082d
valic-m <valic25@gmail.com>
2024-12-31 18:36:53 -0800

    Complete locations-feature with updates

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\valic\OneDrive\Documents\TMS\env
+set VIRTUAL_ENV=C:\Users\valic\Documents\Github\StarmarkTMS\env
 
 if not defined PROMPT set PROMPT=$P$G
 
---
+++ fastapi_app/app/config.py
@@ -0,0 +1,3 @@
+# Configurations for FastAPI app
+PROJECT_NAME = "Starmark TMS FastAPI"
+VERSION = "1.0.0"
